0ffa192ca7e824487be76fafd705bbe9b9ae33c7
FredrikH03 <fredrikholst03@hotmail.com>
2024-02-13 09:13:51 +0100

    Moved work to the new main

---
+++ InsertQuestions.cs
@@ -0,0 +1,48 @@
+using Npgsql;
+using System.IO;
+namespace real_time_horror_group4;
+
+public class InsertQuestions
+{
+
+    NpgsqlConnection _db;
+
+    public dbUri(npgsqlDataSource db)
+    {
+        _db = db;
+    }
+
+    public async Task PopulateQuestions()
+    {
+        const string query = @"INSERT INTO questions(question) VALUES ($1)";
+        string[] questions = File.ReadAllLines("data/questions.txt");
+        for (int i = 0; i < questions.Length; i++)
+        {
+            string question = questions[i];
+            await using (var cmd = _db.CreateCommand(query))
+            {
+                cmd.Paramaters.AddWithValue(question);
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+    }
+
+    public async Task PopulateAnswers()
+    {
+        const string query = @"INSERT INTO answers(answer, question_id) VALUES ($1, $2)";
+        string[] answers = File.ReadAllLines("data/answers.csv");
+        
+        for (int i = 0; i < answers.Length; i++)
+        {
+            string answerLine = answers[i];
+            string[] answerArray = answerLine.Split(",");
+
+            await using (var cmd = _db.CreateCommand(query))
+            {
+                cmd.Paramaters.AddwithValue(answerArray[0]);
+                cmd.Paramaters.AddwithValue(answerArray[1]);
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+}
+}

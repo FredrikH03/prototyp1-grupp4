98f99b8c662165949d8543fe3ddf6079008ad734
Ahmed1 <dooubleeee.a@gmail.com>
2024-02-13 18:08:38 +0100

    Created db

---
+++ DBcreate.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System.Threading.Tasks;
+
+using System.Threading.Tasks;
+
+public class DBcreate
+{
+    NpgsqlConnection _db;
+
+    public DBcreate(NpgsqlConnection db)
+    {
+        _db = db;
+    }
+
+    public DBcreate(NpgsqlDataSource db)
+    {
+    }
+
+    public async Task CreateTable()
+    {
+        await _db.OpenAsync();
+
+        using (var cmd = _db.CreateCommand())
+        {
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS Users(ID SERIAL PRIMARY KEY, username TEXT, password TEXT)";
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        using (var cmd = _db.CreateCommand())
+        {
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS Questions (ID SERIAL PRIMARY KEY, question TEXT)";
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        using (var cmd = _db.CreateCommand())
+        {
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS CorrectAnswers (ID SERIAL PRIMARY KEY, answer TEXT, questionID INT REFERENCES Questions(ID))";
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+}

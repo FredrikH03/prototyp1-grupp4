21a028a8b3532a9e08017893df31c07ba818f2e1
FredrikH03 <fredrikholst03@hotmail.com>
2024-02-15 09:14:15 +0100

    finished basic code to get random guestions and the ability to answer them

--- GetQuestions.cs
+++ GetQuestions.cs
@@ -18,26 +18,35 @@ public class GetQuestions
         Random random = new Random();
         int num = random.Next(1, 31);
 
-        Console.WriteLine(num);
-
         await using var getQuestions = _db.CreateCommand($"SELECT questions FROM Questions WHERE ID = {num}");
         await using var reader = await getQuestions.ExecuteReaderAsync();
         while (await reader.ReadAsync())
         {
             question = reader.GetString(0);
         }
-        Console.WriteLine(question);
+        Console.WriteLine("the riddle is as follows: " + question);
 
         string userInput = Console.ReadLine();
 
         await using var getAnswer =
-            _db.CreateCommand($"SELECT answer FROM CorrectAnswers WHERE ID = {num} AND answer = {userInput}");
+            _db.CreateCommand($"SELECT answer FROM CorrectAnswers WHERE questionid = {num} AND answer = '$1'");
         await using var reader2 = await getAnswer.ExecuteReaderAsync();
         while (await reader2.ReadAsync())
         {
+            getAnswer.Parameters.AddWithValue(userInput);
             answer = reader2.GetString(0);
         }
-        Console.WriteLine(answer);
+        //Console.WriteLine(answer);
+        Console.WriteLine("your guess is...");
+        Thread.Sleep(1000);
+        if (answer == "test")
+        {
+            Console.WriteLine("wrong!!!");
+        }
+        else
+        {
+            Console.WriteLine("Correct!!!");
+        }
     } 
     
 }

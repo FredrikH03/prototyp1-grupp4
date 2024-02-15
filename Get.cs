a09c7ca94556f9c20eeb2ebf76962620afc9f471
Ahmed1 <dooubleeee.a@gmail.com>
2024-02-15 16:02:18 +0100

    get change

--- Get.cs
+++ Get.cs
@@ -1,69 +1,23 @@
-﻿using Npgsql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace real_time_horror_group4;
-
-public class Get(HttpListenerRequest request, NpgsqlDataSource db)
+namespace real_time_horror_group4
 {
-    public string? path = request.Url.AbsolutePath;
-    public string? Lastpath = request.Url.AbsolutePath.Split("/").Last();
-
-    public string test()
+    public class GET(HttpListenerRequest request)
     {
 
-        string test = "test";
-
-        return test;
-
-    }
-     
-    public string getter()// vi kör denna typen av metoden för vi vill returna string, void returnar inte.
-    {
-        if(path != null)
-        {
-            if (path.Contains("/test"))
-            {
-                return test(); 
-
-
-            }
-            if (path.Contains("/leaderboard"))
-            {
-
-
-                return leaderboard(); // här vill vi return senaste metoden.
-
-
-            }
-
-
-
-        }
-
-
-
-        return "not found";
-
-    }
-
-
-     
-    
-
-    public string leaderboard()
-    {
-        string key = "melih";
-
-        return key;
-    }
         
-    
 
 
+
+
+
+
+
+
+    }
 }

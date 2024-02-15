0a6b9c19527af6e2abe60a54f537aa1e96b22a1a
FredrikH03 <fredrikholst03@hotmail.com>
2024-02-15 10:17:01 +0100

    for some reason the entire file was commented out

--- InsertInfo.cs
+++ InsertInfo.cs
@@ -2,46 +2,46 @@ using Npgsql;
 using System.IO;
 namespace real_time_horror_group4;
 
-//public class InsertInfo
-//{
+public class InsertInfo
+{
     
-//    private readonly NpgsqlDataSource _db;
-//    public InsertInfo(NpgsqlDataSource db)
-//    {
-//        _db = db;
-//    }
+    private readonly NpgsqlDataSource _db;
+    public InsertInfo(NpgsqlDataSource db)
+    {
+        _db = db;
+    }
 
-//    public async Task PopulateQuestions()
-//    {
-//        const string query = @"INSERT INTO Questions(questions) VALUES ($1)";
-//        string[] questions = File.ReadAllLines("data/questions.txt");
-//        for (int i = 0; i < questions.Length; i++)
-//        {
-//            string question = questions[i];
-//            await using (var cmd = _db.CreateCommand(query))
-//            {
-//                cmd.Parameters.AddWithValue(question);
-//                await cmd.ExecuteNonQueryAsync();
-//            }
-//        }
-//    }
+    public async Task PopulateQuestions()
+    {
+        const string query = @"INSERT INTO Questions(questions) VALUES ($1)";
+        string[] questions = File.ReadAllLines("data/questions.txt");
+        for (int i = 0; i < questions.Length; i++)
+        {
+            string question = questions[i];
+           await using (var cmd = _db.CreateCommand(query))
+            {
+                cmd.Parameters.AddWithValue(question);
+                await cmd.ExecuteNonQueryAsync();
+            }
+       }
+    }
 
-//    public async Task PopulateAnswers()
-//    {
-//        const string query = @"INSERT INTO CorrectAnswers(answer, questionID) VALUES ($1, $2)";
-//        string[] answers = File.ReadAllLines("data/answers.csv");
-        
-//        for (int i = 0; i < answers.Length; i++)
-//        {
-//            string answerLine = answers[i];
-//            string[] answerArray = answerLine.Split(",");
+   public async Task PopulateAnswers()
+    {
+        const string query = @"INSERT INTO CorrectAnswers(answer, questionID) VALUES ($1, $2)";
+        string[] answers = File.ReadAllLines("data/answers.csv");
+      
+        for (int i = 0; i < answers.Length; i++)
+        {
+           string answerLine = answers[i];
+            string[] answerArray = answerLine.Split(",");
 
-//            await using (var cmd = _db.CreateCommand(query))
-//            {
-//                cmd.Parameters.AddWithValue(answerArray[0]);
-//                cmd.Parameters.AddWithValue(int.Parse(answerArray[1]));
-//                await cmd.ExecuteNonQueryAsync();
-//            }
-//        }
-//}
-//}
+            await using (var cmd = _db.CreateCommand(query))
+            {
+                cmd.Parameters.AddWithValue(answerArray[0]);
+                cmd.Parameters.AddWithValue(int.Parse(answerArray[1]));
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+}
+}
